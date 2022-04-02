598150d905449ff955b5fe7c39e78e754514d21e
Paulo-Pinto <paulopintodigital@gmail.com>
2022-04-02 13:13:39 +0100

    + test coverage

--- tests/tests.py
+++ tests/tests.py
@@ -4,59 +4,101 @@ import main.main as _
 
 
 class MyTestCase(unittest.TestCase):
-    def test_gameOverLeft(self):
+    def test_initGameOver(self):
         state = _.State([1, 1, 0])
-        self.assertTrue(_.isGameOver(state))
-
-    def test_gameOverRight(self):
+        with self.subTest():
+            self.assertTrue(_.isGameOver(state))
         state = _.State([], [1, 1, 0])
-        self.assertTrue(_.isGameOver(state))
+        with self.subTest():
+            self.assertTrue(_.isGameOver(state))
 
-    def test_gameNotOver1(self):
+    def test_initGameNotOver(self):
         state = _.State([1, 1, 0, 0], [0, 1])
-        self.assertFalse(_.isGameOver(state))
+        with self.subTest():
+            self.assertFalse(_.isGameOver(state))
 
-    def test_gameNotOver2(self):
         state = _.State([1, 0], [1, 1, 0, 0])
-        self.assertFalse(_.isGameOver(state))
+        with self.subTest():
+            self.assertFalse(_.isGameOver(state))
 
     def test_move(self):
         state = _.State()
         state.move(1, 1)
-        self.assertTrue(state.right == [1, 1])
+        with self.subTest():
+            self.assertTrue(state.right == [1, 1] and state.left == [0, 0, 0, 1])
+
+        state.move(1, 1)
+        with self.subTest():
+            self.assertTrue(state.right == [] and state.left == [0, 0, 0, 1, 1, 1])
+
+        with self.subTest():
+            self.assertFalse(state.move(0, 0))  # move into game over
+
+    def test_incompatibleMove(self):
+        state = _.State()
+
+        with self.subTest():
+            self.assertFalse(state.move())  # empty move
+
+        state.move(1, 1)
+
+        with self.subTest():
+            self.assertFalse(state.move(0))  # incompatible move of 1 person
+
+        with self.subTest():
+            self.assertFalse(state.move(0, 0))  # incompatible move of 2 same people
+
+        with self.subTest():
+            self.assertFalse(state.move(0, 1))  # incompatible move of 2 diff people
 
     def test_gameWin(self):
         self.assertTrue(_.isWin(_.State([], [1, 1, 1, 0, 0, 0])))
 
     def test_printState(self):
         state = _.State()
-        self.assertEqual(str(state.showStateSimple()), "([0, 0, 0, 1, 1, 1], '_ ', [])")
 
-    def test_printStatePretty(self):
-        state = _.State()
-        self.assertEqual(str(state.showStatePretty()), "🕋, 🕋, 🕋, 👿, 👿, 👿 🚤 ___ ")
+        with self.subTest():
+            self.assertEqual(str(state.showStateSimple()), "([0, 0, 0, 1, 1, 1], '_ ', [])")
 
-    def test_TrueWin(self):
+        with self.subTest():
+            self.assertEqual(str(state.showStatePretty()), "🕋, 🕋, 🕋, 👿, 👿, 👿 🚤 ___ ")
+
+    def test_winningMoves(self):
         state = _.State()
-        state.move(1, 1)
-        state.move(1)
-        state.move(1, 1)
-        state.move(1)
-        state.move(0, 0)
-        state.move(0, 1)
-        state.move(0, 0)
-        state.move(1)
-        state.move(1, 1)
-        state.move(1)
-        state.move(1, 1)
+
+        move_sequence = [
+            [1, 1],
+            [1],
+            [1, 1],
+            [1],
+            [0, 0],
+            [0, 1],
+            [0, 0],
+            [1],
+            [1, 1],
+            [1],
+            [1, 1],
+        ]
+
+        [state.move(*m) for m in move_sequence]
+
         self.assertTrue(_.isWin(state))
 
-    def test_FalseWin(self):
+    def test_notWin(self):
         state = _.State()
         self.assertFalse(_.isWin(state))
 
+    def test_manualPlayGameOver(self):
+        state = _.State([0, 1, 1])
+        self.assertFalse(_.manualPlay(state))
+
+    def test_manualPlayWin(self):
+        state = _.State([], [0, 0, 0, 1, 1, 1])
+        self.assertTrue(_.manualPlay(state))
+
+
+# TODO : test manualPlay() with mock input
+# TODO : test solve (if it always wins)
 
 if __name__ == "__main__":
     unittest.main()
-
-# TODO : more tests
